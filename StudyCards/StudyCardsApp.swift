70fd80ca5f5fddd192928e20ef9ba96825c406b5
brunocoronado49 <jofrancoracor@gmail.com>
2021-09-04 17:18:52 -0500

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by Bruno Coronado on 04/09/21.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
